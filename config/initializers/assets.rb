58f26e39c860bc329d226bb26ced6c3768658ed6
karlxavier <kxdc1025@yahoo.com>
2017-03-01 00:54:57 +0400

    added car show details with image slider

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,4 +9,6 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
+# Rails.application.config.assets.precompile += %w( .js .css *.css.scss .svg .eot .woff .ttf)
 Rails.application.config.assets.precompile += %w( application-index.css )
+
